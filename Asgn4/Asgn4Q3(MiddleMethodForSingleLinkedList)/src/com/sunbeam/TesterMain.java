acea3ccff0b63383183b4498976d9cb6da455d67
kd1-80101 <prasannshekhar@gmail.com>
2023-10-30 19:29:20 +0530

    asgn4 Q3 uploaded

---
+++ Asgn4/Asgn4Q3(MiddleMethodForSingleLinkedList)/src/com/sunbeam/TesterMain.java
@@ -0,0 +1,18 @@
+package com.sunbeam;
+
+public class TesterMain {
+
+	public static void main(String[] args) {
+		SinglyLinkendList sll = new SinglyLinkendList();
+
+//		sll.addFirst(50);
+		sll.addFirst(40);
+		sll.addFirst(30);
+		sll.addFirst(20);
+		sll.addFirst(10);
+
+		int data = sll.middleElementMethod();
+		System.out.println("Middle element : " + data);
+	}
+
+}
