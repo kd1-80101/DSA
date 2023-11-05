5295607f941cbc2452d0e3f15cbb3f2980161066
kd1-80101 <prasannshekhar@gmail.com>
2023-11-05 10:18:32 +0530

    asgn4 Q8 uploaded

---
+++ Asgn4/Asgn4Q6/Asgn4Q8/src/com/sunbeam/TesterMain.java
@@ -0,0 +1,15 @@
+package com.sunbeam;
+
+public class TesterMain {
+	public static void main(String[] args) {
+		SinglyLinearLinkedList list = new SinglyLinearLinkedList();
+		list.addSorted(10);
+		list.addSorted(5);
+		list.addSorted(14);
+		list.addSorted(12);
+		list.addSorted(20);
+		list.addSorted(16);
+
+		list.display();
+	}
+}
